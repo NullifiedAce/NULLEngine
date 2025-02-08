3abd63a87fe53c803f9d6f6ea08a08a25de5d23f
NULLSonic <b0yfr13ndhihi@gmail.com>
2025-02-08 18:45:29 +0100

    Credits changes

--- scenes/TitleScreen.gd
+++ scenes/TitleScreen.gd
@@ -83,7 +83,7 @@ func beat_hit(beat:int):
 	match beat:
 		1:
 			create_cool_text(['nullsonic'])
-			add_more_text('cilysong')
+			add_more_text('cilystuff')
 		3:
 			add_more_text('presents')
 		4:
