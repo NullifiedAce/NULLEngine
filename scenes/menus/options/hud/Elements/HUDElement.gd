9ec8956e57c0fa70f234f8873e65c60df3da9704
NULLSonic <b0yfr13ndhihi@gmail.com>
2025-04-09 20:22:57 +0200

    HUD Editor Revamp - Part 2
    HUDLabel is almost done, can now add multiple HUDElements to the screen.

--- scenes/menus/options/hud/Elements/HUDElement.gd
+++ scenes/menus/options/hud/Elements/HUDElement.gd
@@ -5,22 +5,26 @@ var hud_label:HUDLabel
 
 var update_values:bool = true
 
-@onready var default_children: VBoxContainer = $VBoxContainer
-
 @onready var config: Button = $Config
 @onready var move: Button = $Move
 @onready var configure_window: Window = $Configure
 
 @onready var options: VBoxContainer = $Configure/ScrollContainer/VBoxContainer
 
-@onready var type: OptionButton = $Configure/ScrollContainer/VBoxContainer/HBoxContainer/Type
+@onready var default_options: VBoxContainer = $'Configure/ScrollContainer/VBoxContainer/Default Options'
+@onready var type_options: VBoxContainer = $'Configure/ScrollContainer/VBoxContainer/Type Options' # HUD Elements options will be moved in here.
 
-@onready var x_pos_box: SpinBox = $Configure/ScrollContainer/VBoxContainer/HBoxContainer2/XPos
-@onready var y_pos_box: SpinBox = $Configure/ScrollContainer/VBoxContainer/HBoxContainer2/YPos
+@onready var type: OptionButton = $'Configure/ScrollContainer/VBoxContainer/Default Options/HBoxContainer/Type'
+
+@onready var x_pos_box: SpinBox = $'Configure/ScrollContainer/VBoxContainer/Default Options/HBoxContainer2/XPos'
+@onready var y_pos_box: SpinBox = $'Configure/ScrollContainer/VBoxContainer/Default Options/HBoxContainer2/YPos'
+@onready var donwscroll_box: SpinBox = $'Configure/ScrollContainer/VBoxContainer/Default Options/Downscroll SpinBox'
 
 func _ready() -> void:
 	config.pressed.connect(_config_window)
 	type.item_selected.connect(_update_type)
+
+	_update_type(0)
 
 func _process(delta: float) -> void:
 	if move.button_pressed:
@@ -30,23 +34,23 @@ func _process(delta: float) -> void:
 		x_pos_box.value = position.x
 		y_pos_box.value = position.y
 	else:
-		if !update_values:
-			return
-		else:
-			position = Vector2(x_pos_box.value, y_pos_box.value)
+		position = Vector2(x_pos_box.value, y_pos_box.value)
 
 	if position.y >= 660: move.position.y = -48
 	else: move.position.y = 48
 
 func _update_type(index:int):
+	for i in type_options.get_children():
+		i.queue_free()
 	if index == 0:
 		if hud_label != null: hud_label.queue_free()
 		hud_label = load("res://scenes/menus/options/hud/Elements/HUDLabel.tscn").instantiate()
 		add_child(hud_label)
 		for i in hud_label.get_children():
-			print("hi")
-			i.reparent(options)
-			i.show()
+			i.reparent(type_options)
+			if i is not Window: i.show()
+	if index == 1:
+		if hud_label != null: hud_label.queue_free()
 
 func _config_window():
 	if configure_window.visible: configure_window.hide()
--- scenes/menus/options/hud/Menu.gd
+++ scenes/menus/options/hud/Menu.gd
@@ -1,4 +1,5 @@
 extends MusicBeatScene
+class_name HUDEditor
 
 @onready var file_menu: MenuButton = $File
 @onready var edit_menu: MenuButton = $Edit
@@ -8,6 +9,9 @@ extends MusicBeatScene
 @onready var preferences_window: Window = $Preferences
 @onready var add_popup: PopupMenu = $AddPopup
 
+@onready var hud_template: HUDElement = $Template
+
+
 func _ready() -> void:
 	for i in FPS.get_children():
 		i.modulate = Color(1, 1, 1, 0.25)
@@ -15,6 +19,7 @@ func _ready() -> void:
 	file_menu.get_popup().id_pressed.connect(_file_stuff)
 	edit_menu.get_popup().id_pressed.connect(_edit)
 	options_menu.get_popup().id_pressed.connect(_options)
+	add_popup.id_pressed.connect(_add_stuff)
 
 func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("hud_exit"):
@@ -43,6 +48,15 @@ func _options(id:int):
 		"Use downscroll?":
 			var is_pressed = not options_menu.get_popup().is_item_checked(id)
 			options_menu.get_popup().set_item_checked(id, is_pressed)
+
+func _add_stuff(id:int):
+	match add_popup.get_item_text(id):
+		"Add HUD Element here":
+			var new_hud = load("res://scenes/menus/options/hud/Elements/HUDElement.tscn").instantiate()
+			add_child(new_hud)
+
+			new_hud.x_pos_box.value = get_viewport().get_mouse_position().x
+			new_hud.y_pos_box.value = get_viewport().get_mouse_position().y
 
 func _exit_tree() -> void:
 	for i in FPS.get_children():
