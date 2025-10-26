a08c3bd2a84f0aec4291f626d21a189e41591e4e
NullifiedAce <b0yfr13ndhihi@gmail.com>
2025-10-26 13:35:07 +0100

    Satin Panties Erect Fix + MacOS Video Ignore

--- scenes/gameplay/cutscenes/video-example.gd
+++ scenes/gameplay/cutscenes/video-example.gd
@@ -2,6 +2,8 @@ extends Cutscene
 @onready var video = $video
 
 func _ready():
+	if OS.get_name() == "macOS": # Skip video cutscenes as they don't work correctly on MacOS.
+		_end()
 	video.finished.connect(_end)
 	video.play()
 
